2fab9d9b0eecf7d8bdbab89a5fb8213eafb84db6
Rocco Bowling <rocco@chimerasw.com>
2024-02-19 17:05:59 -0500

    operation queue

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # MailPacket
-Flynn enabled wrapper around libetpan
+
+Minimal Flynn-enabled wrapper around libetpan to perform basic mail functionality:
+
+- connect
+- search
+- download headers
+- download eml
--- Sources/MailPacket/IMAP.swift
+++ Sources/MailPacket/IMAP.swift
@@ -19,13 +19,17 @@ public struct Email: Codable {
 }
 
 public class IMAP: Actor {
-    
+    private let queue: OperationQueue
+
     private let imap: UnsafeMutableRawPointer?
     private let domain: String
     private let port: Int
     
     public init(domain: String,
                 port: Int) {
+        queue = OperationQueue()
+        queue.maxConcurrentOperationCount = 1
+
         imap = cmailimap_new()
         self.domain = domain
         self.port = port
@@ -34,84 +38,94 @@ public class IMAP: Actor {
     internal func _beConnect(account: String,
                              password: String,
                              _ returnCallback: @escaping (String?) -> ()) {
-        var result: CError = cmailimap_ssl_connect(self.imap, "imap.gmail.com", 993)
-        if let error = result.toString() {
-            return returnCallback(error)
+        queue.addOperation {
+            var result: CError = cmailimap_ssl_connect(self.imap, "imap.gmail.com", 993)
+            if let error = result.toString() {
+                return returnCallback(error)
+            }
+            
+            result = cmailimap_login(self.imap, account, password)
+            if let error = result.toString() {
+                return returnCallback(error)
+            }
+            
+            returnCallback(nil)
         }
-        
-        result = cmailimap_login(self.imap, account, password)
-        if let error = result.toString() {
-            return returnCallback(error)
-        }
-        
-        returnCallback(nil)
     }
     
     internal func _beSelect(folder: String,
                             _ returnCallback: @escaping (String?) -> ()) {
-        let result: CError = cmailimap_select(self.imap, "INBOX")
-        if let error = result.toString() {
-            return returnCallback(error)
+        queue.addOperation {
+            let result: CError = cmailimap_select(self.imap, "INBOX")
+            if let error = result.toString() {
+                return returnCallback(error)
+            }
+            
+            returnCallback(nil)
         }
-        
-        returnCallback(nil)
     }
     
     internal func _beSearch(folder: String,
                             after: Date,
                             smaller: Int = 0,
                             _ returnCallback: @escaping (String?, [Int]) -> ()) {
-        let result: CError = cmailimap_select(self.imap, "INBOX")
-        if let error = result.toString() {
-            return returnCallback(error, [])
+        queue.addOperation {
+            let result: CError = cmailimap_select(self.imap, "INBOX")
+            if let error = result.toString() {
+                return returnCallback(error, [])
+            }
+            
+            let calendarDate = Calendar.current.dateComponents([.day, .year, .month], from: after)
+            guard let day = calendarDate.day,
+                  let month = calendarDate.month,
+                  let year = calendarDate.year else {
+                return returnCallback("failed to extract date components", [])
+            }
+            if let jsonUTF8 = cmailimap_search(self.imap,
+                                               Int32(day),
+                                               Int32(month),
+                                               Int32(year),
+                                               Int32(smaller)) {
+                let json = Hitch(own: jsonUTF8)
+                let messageIDs: [Int] = json.query("$[*]") ?? []
+                return returnCallback(nil, messageIDs)
+            }
+            
+            return returnCallback("unknown error", [])
         }
-        
-        let calendarDate = Calendar.current.dateComponents([.day, .year, .month], from: after)
-        guard let day = calendarDate.day,
-              let month = calendarDate.month,
-              let year = calendarDate.year else {
-            return returnCallback("failed to extract date components", [])
-        }
-        if let jsonUTF8 = cmailimap_search(self.imap,
-                                           Int32(day),
-                                           Int32(month),
-                                           Int32(year),
-                                           Int32(smaller)) {
-            let json = Hitch(own: jsonUTF8)
-            let messageIDs: [Int] = json.query("$[*]") ?? []
-            return returnCallback(nil, messageIDs)
-        }
-        
-        return returnCallback("unknown error", [])
     }
     
     internal func _beHeaders(messageIDs: [Int],
                              _ returnCallback: @escaping (String?, [Header]) -> ()) {
-        var cMessageIDs = messageIDs.map { Int32($0) }
-        
-        if let jsonUTF8 = cmailimap_headers(self.imap,
-                                            Int32(messageIDs.count),
-                                            &cMessageIDs) {
-            let json = Hitch(own: jsonUTF8)
-            let headers: [Header] = json.query("$[*]") ?? []
-            return returnCallback(nil, headers)
+        queue.addOperation {
+            var cMessageIDs = messageIDs.map { Int32($0) }
+            
+            if let jsonUTF8 = cmailimap_headers(self.imap,
+                                                Int32(messageIDs.count),
+                                                &cMessageIDs) {
+                let json = Hitch(own: jsonUTF8)
+                let headers: [Header] = json.query("$[*]") ?? []
+                return returnCallback(nil, headers)
+            }
+            
+            return returnCallback("unknown error", [])
         }
-        
-        return returnCallback("unknown error", [])
     }
     
     internal func _beDownload(messageIDs: [Int],
                               _ returnCallback: @escaping (String?, [Email]) -> ()) {
-        var cMessageIDs = messageIDs.map { Int32($0) }
-        
-        if let jsonUTF8 = cmailimap_download(self.imap,
-                                             Int32(messageIDs.count),
-                                             &cMessageIDs) {
-            let json = Hitch(own: jsonUTF8)
-            let emails: [Email] = json.query("$[*]") ?? []
-            return returnCallback(nil, emails)
+        queue.addOperation {
+            var cMessageIDs = messageIDs.map { Int32($0) }
+            
+            if let jsonUTF8 = cmailimap_download(self.imap,
+                                                 Int32(messageIDs.count),
+                                                 &cMessageIDs) {
+                let json = Hitch(own: jsonUTF8)
+                let emails: [Email] = json.query("$[*]") ?? []
+                return returnCallback(nil, emails)
+            }
+            
+            return returnCallback("unknown error", [])
         }
-        
-        return returnCallback("unknown error", [])
     }
 }
